ef4ce866c8335ee1c4956b693b328c7a9dc09d12
Severine Dubois <sevdbs@gmail.com>
2022-05-11 16:52:08 +0200

    localstorage themeDark ok

--- public/assets/js/changeTheme.js
+++ public/assets/js/changeTheme.js
@@ -1,5 +1,6 @@
 const changeTheme = {
     init: function() {
+    changeTheme.saveToLocalStorage();
     changeTheme.dark();
     changeTheme.light();
 },
@@ -7,17 +8,21 @@ const changeTheme = {
     dark: function() {
 
         const dark = document.querySelector(".gengar");
-        console.log(dark);
+        // console.log(dark);
       
         dark.addEventListener("click", function() {
-      
-          const change = document.getElementsByClassName("change-theme");
-          console.log(change);
-      
-          for (const changeToDark of change) {
-              changeToDark.classList.add("dark-theme"); 
-          }
-      
+
+        const change = document.querySelectorAll(".change-theme");
+        // console.log(change);
+        
+        for (const changeToDark of change) {
+                changeToDark.classList.add("dark-theme"); 
+        }
+
+          localStorage.setItem("dark-mode", "enabled");
+          // const isDark = localStorage.getItem("dark-mode");
+          //console.log(isDark);
+          
         });
     },
       
@@ -25,17 +30,35 @@ const changeTheme = {
         const pika = document.querySelector(".pika");
       
           pika.addEventListener("click", function() {
-              const darkclass = document.querySelectorAll(".dark-theme");
-              console.log(darkclass);
+              const darkclass = document.querySelectorAll(".change-theme");
+              // console.log(darkclass);
       
       
              for (const darkclassRemove of darkclass) {
               darkclassRemove.classList.remove("dark-theme");
             };
     
+            localStorage.setItem("dark-mode", "disabled");
+
             });
         
-        }
+        },
+
+        saveToLocalStorage: function (){
+    
+            const isDark = localStorage.getItem("dark-mode");
+            // console.log(isDark);
+    
+            if(isDark === "enabled"){
+                const change = document.querySelectorAll(".change-theme");
+                // console.log(change);
+            
+                for (const changeToDark of change) {
+                    changeToDark.classList.add("dark-theme"); 
+                }
+            }
+    
+        },
         
 }
 
